43fa01208e45cade28eb9b40d773f942d9d2a545
Khris Punzalan <khrispunzalan@gmail.com>
2020-01-23 20:01:56 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
 
-// Write your code here
+let multiply = num1 * num2;
+// return multiply;
+
+let random = Math.random() + 1;
+
+let mod = 64 % 10;
+
+let max = Math.max(1, 2, 3, 4, 20);
